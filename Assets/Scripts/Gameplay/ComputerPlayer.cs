2fabb1a4bafadf843ab91d14d9b734c65d2881cd
John Bergsten <putte_dt@hotmail.se>
2020-08-04 08:27:42 +0200

    BAS-11: Add AI class. Did some restructuring

---
+++ Assets/Scripts/Gameplay/ComputerPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerPlayer : MonoBehaviour
+{
+
+    private Player m_player;
+    public Transform m_debugMoveToPosition;
+    // Use this for initialization
+    void Start()
+    {
+        m_player = GetComponent<Player>();
+        StartCoroutine("M_StartMove");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private IEnumerator M_StartMove()
+    {
+        yield return new WaitForSeconds(2.0f);
+        m_player.M_MoveOrder(m_debugMoveToPosition.position);
+    }
+}
